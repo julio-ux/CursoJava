0a622e3bbab4db33b773283c9eea531d37fd0321
julio_castillo <julio_castillo19922005@outlook.com>
2021-12-13 06:41:51 -0600

    Curso Java Video 13

---
+++ primerospasos/src/primerospasos/V012_Pruebas.java
@@ -0,0 +1,15 @@
+package primerospasos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class V012_Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre;
+		Scanner miobjeto;
+		Locale miobjeto2;
+	}
+
+}
