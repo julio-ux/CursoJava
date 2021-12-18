042db7690797a13da8ff1fe94b4cb8115b853d3c
julio_castillo <julio_castillo19922005@outlook.com>
2021-12-18 11:53:19 -0600

    Video 15 entrada de datos

---
+++ primerospasos/src/primerospasos/V014_1_Entrada_Ejemplo1.java
@@ -0,0 +1,15 @@
+package primerospasos;
+import java.util.*;;
+public class V014_1_Entrada_Ejemplo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("¿Cual es tu nombre?");
+		String nombre = teclado.nextLine();
+		System.out.println("¿Cual es tu edad?");
+		int edad = teclado.nextInt();
+		System.out.println("Tu nombre es: " + nombre + " el año que viene tendras: " + (edad + 1) + " años");
+	}
+
+}
