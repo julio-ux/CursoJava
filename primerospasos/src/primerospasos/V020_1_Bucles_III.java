b20d394ef5b2e63d0dd504b39e4eee789fcc01df
julio_castillo <julio_castillo19922005@outlook.com>
2021-12-29 05:45:10 -0600

    Video 20 Java Bucle do while

---
+++ primerospasos/src/primerospasos/V020_1_Bucles_III.java
@@ -0,0 +1,20 @@
+package primerospasos;
+import javax.swing.*;
+public class V020_1_Bucles_III {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String genero = "";
+		do {
+			genero = JOptionPane.showInputDialog("¿Cual es su genero (H/M)?");
+		}while(!genero.equalsIgnoreCase("H") && !genero.equalsIgnoreCase("M"));
+		
+		int altura = Integer.parseInt(JOptionPane.showInputDialog("¿Cual es su altura? en cm"));
+		if(genero.equalsIgnoreCase("H")) {	
+			System.out.println("Su peso ideal es: " + (altura - 110) + " kg");
+		}else {
+			System.out.println("Su peso ideal es: " + (altura - 120) + " kg");
+		}
+	}
+
+}
