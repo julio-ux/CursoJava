f95baa26af1428b2141ae82122f40ab3bf4ed53d
julio-ux <72508490+julio-ux@users.noreply.github.com>
2022-01-23 07:13:39 -0600

    Curso Java. POO II. Vídeo 28

---
+++ primerospasos/src/poo/V028_POO_II_Coche.java
@@ -0,0 +1,17 @@
+package poo;
+
+public class V028_POO_II_Coche {
+	int ruedas;
+	int largo;
+	int ancho;
+	int motor;
+	int peso;
+	
+	public V028_POO_II_Coche() {
+		this.ruedas = 4;
+		this.largo = 2000;
+		this.ancho = 300;
+		this.motor = 1600;
+		this.peso = 500;
+	}
+}
