57008a886d32e9dcaa1e5db376202a27dadc9881
julio_castillo <julio_castillo19922005@outlook.com>
2022-03-10 09:18:28 -0600

    Curso Java POO IX. Construcción objetos IV. Vídeo 35

--- primerospasos/src/poo/Uso_Empleado.java
+++ primerospasos/src/poo/Uso_Empleado.java
@@ -2,15 +2,50 @@ package poo;
 
 import java.util.*;
 
+import javax.swing.JOptionPane;
+
 public class Uso_Empleado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Empleado Julio = new Empleado("Julio",2000,2019,8,21);
-		System.out.println("El nombre del empleado es: " + Julio.getNombre());
-		Julio.setAumentaSueldo(5);
-		System.out.println("El sueldo de este empleado es: $" + Julio.getSueldo());
-		System.out.println("y la fecha de alta es: " + Julio.getFecha());
+//		Empleado empleado1 = new Empleado("Paco Gómez",85000,1990,12,17);
+//		Empleado empleado2 = new Empleado("Anna López",95000,1995,6,2);
+//		Empleado empleado3 = new Empleado("Maria Martín",105000,2002,3,15);
+//		
+//		empleado1.setAumentaSueldo(5);
+//		empleado2.setAumentaSueldo(5);
+//		empleado3.setAumentaSueldo(5);
+//		
+//		System.out.println("Nombre: " + empleado1.getNombre() + " "
+//				+ "Sueldo: " + empleado1.getSueldo() + " "
+//				+ "Fecha de alta: " + empleado1.getFecha());
+//		
+//		System.out.println("Nombre: " + empleado2.getNombre() + " "
+//				+ "Sueldo: " + empleado2.getSueldo() + " "
+//				+ "Fecha de alta: " + empleado2.getFecha());
+//		
+//		System.out.println("Nombre: " + empleado3.getNombre() + " "
+//				+ "Sueldo: " + empleado3.getSueldo() + " "
+//				+ "Fecha de alta: " + empleado3.getFecha());
+		int nEmpleados = Integer.parseInt(JOptionPane.showInputDialog("¿Cual es el numero de empleados?"));
+		Empleado empleados [] = new Empleado[nEmpleados];
+		for (int i = 0; i < empleados.length; i++) {
+			String nombre = JOptionPane.showInputDialog("Nombre del empleado " + (i + 1) + ":");
+			double sueldo = Double.parseDouble(JOptionPane.showInputDialog("Sueldo del empleado " + (i + 1) + ":"));
+			int dia = Integer.parseInt(JOptionPane.showInputDialog("Dia de ingreso del empleado " + (i + 1) + ":"));
+			int mes = Integer.parseInt(JOptionPane.showInputDialog("Mes de ingreso del empleado " + (i + 1) + ":"));
+			int agno = Integer.parseInt(JOptionPane.showInputDialog("Año de ingreso del empleado " + (i + 1) + ":"));
+			empleados[i] = new Empleado(nombre,sueldo,dia,mes,agno);
+		}
+		
+		for(Empleado e : empleados) {
+			e.setAumentaSueldo(5);
+			System.out.println("Nombre: " + e.getNombre() + " "
+					+ "Sueldo: " + e.getSueldo() + " "
+					+ "Fecha de alta: " + e.getFecha());
+		}
+		
+		
 	}
 
 }
